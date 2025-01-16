373981f8b9a0b3584b09fcff68a208046eb3ca87
SophiaDLM <SophiaDLM@outlook.es>
2025-01-16 04:39:30 +0100

    Commit inicial - Proyecto vacío

---
+++ Tarea3ADSophiaDeLucaMiranda/src/main/java/com/sophiadlm/Tarea3ADSophiaDeLucaMiranda/Tarea3AdSophiaDeLucaMirandaApplication.java
@@ -0,0 +1,13 @@
+package com.sophiadlm.Tarea3ADSophiaDeLucaMiranda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea3AdSophiaDeLucaMirandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea3AdSophiaDeLucaMirandaApplication.class, args);
+	}
+
+}
---
+++ Tarea3ADSophiaDeLucaMiranda/src/test/java/com/sophiadlm/Tarea3ADSophiaDeLucaMiranda/Tarea3AdSophiaDeLucaMirandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophiadlm.Tarea3ADSophiaDeLucaMiranda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3AdSophiaDeLucaMirandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
